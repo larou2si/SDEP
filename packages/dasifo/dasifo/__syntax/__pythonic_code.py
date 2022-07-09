3b23d36e6a115563b526a5233d76a1ed8beebd67
laroussi mohamed <47029450+larou2si@users.noreply.github.com>
2022-07-10 00:19:58 +0100

    how to use context manager

--- packages/dasifo/dasifo/__syntax/__pythonic_code.py
+++ packages/dasifo/dasifo/__syntax/__pythonic_code.py
@@ -44,3 +44,19 @@ def outer():
     print(x)
 outer()
 print(x)
+
+
+# ------- START contextmanager ----------------
+from contextlib import contextmanager
+
+@contextmanager
+def open_file(file, mode):
+    f = open(file, mode)
+    yield f
+    f.close()
+
+with open_file('test.txt', 'w') as f:
+    f.write('sample text')
+
+print(f.closed) # -- True
+# ------- END contextmanager ----------------
